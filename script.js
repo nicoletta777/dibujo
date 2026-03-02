6c30186d529e2319545a507c407852a1592f553e
nicoletta777 <n.parras23@uniandes.edu.co>
2026-03-02 16:46:38 +0000

    cambios

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const varTitulo = document.getElementById("titulo");
+
+varTitulo.addEventListener("click", () => {
+    varTitulo.innerText= "v0dk4"
+    varTitulo.style.backgroundColor= "pink";
+})
+
